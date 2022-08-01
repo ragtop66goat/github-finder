473216fbee84c091c496494692990e8b6a298503
Jason Gionfriddo <Ragtop66goat@gmail.com>
2022-08-01 10:58:06 -0500

    Test SSH

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main className="container mx-auto px-3 pb-12">Content</main>
+        <main className="container mx-auto px-3 pb-12">
+          Content Will Go Here
+        </main>
         <Footer />
       </div>
     </Router>
