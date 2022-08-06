06c83b412bdfb2f0ffc225f0127cf16356f8e994
Jason Gionfriddo <Ragtop66goat@gmail.com>
2022-08-05 20:39:28 -0500

    added UserItem, UserResults components. Added About, Home, and NotFound pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p>
+        An application built with react that will search Github for user
+        profiles using ReactRouter v6
+      </p>
+    </div>
+  );
+}
+
+export default About;
